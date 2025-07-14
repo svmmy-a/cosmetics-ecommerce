8512085741ba1bfb9aad2a0bce8d169aeb606618
Samin Ahmed <s.ahmedd99@gmail.com>
2025-07-14 11:53:30 +0100

    setup Spring Boot project structure and initial config

---
+++ src/main/java/com/cosmetics/CosmeticsApplication.java
@@ -0,0 +1,12 @@
+// src/main/java/com/cosmetics/CosmeticsApplication.java
+package com.cosmetics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmeticsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CosmeticsApplication.class, args);
+    }
+}
