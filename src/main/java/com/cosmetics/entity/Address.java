2c14a44036c9fb4f1707254b59ba310c936fe021
Samin Ahmed <s.ahmedd99@gmail.com>
2025-07-15 01:41:05 +0100

    add Address entity with fields for customer, address line, city, postcode, and country

---
+++ src/main/java/com/cosmetics/entity/Address.java
@@ -0,0 +1,102 @@
+package com.cosmetics.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "address")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "address_id")
+    private Integer addressId;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+
+    @Column(name = "address_line", nullable = false)
+    private String addressLine;
+
+    @Column(name = "city", nullable = false)
+    private String city;
+
+    @Column(name = "postcode", nullable = false)
+    private String postcode;
+
+    @Column(name = "country", nullable = false)
+    private String country;
+
+    // Constructors
+    public Address() {
+    }
+
+    public Address(Customer customer, String addressLine, String city, String postcode, String country) {
+        this.customer = customer;
+        this.addressLine = addressLine;
+        this.city = city;
+        this.postcode = postcode;
+        this.country = country;
+    }
+
+    // Getters and Setters
+    public Integer getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(Integer addressId) {
+        this.addressId = addressId;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public String getAddressLine() {
+        return addressLine;
+    }
+
+    public void setAddressLine(String addressLine) {
+        this.addressLine = addressLine;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "addressId=" + addressId +
+                ", customerId=" + (customer != null ? customer.getCustomerId() : null) +
+                ", addressLine='" + addressLine + '\'' +
+                ", city='" + city + '\'' +
+                ", postcode='" + postcode + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
+}
