1797b26a2e47f9d79c0f1b28b75bdbd151e70e9a
Samin Ahmed <s.ahmedd99@gmail.com>
2025-07-26 13:10:23 +0100

    remove unused LocalDateTime import from AnalyticsService and AnalyticsServiceImpl

--- src/main/java/com/cosmetics/service/AnalyticsService.java
+++ src/main/java/com/cosmetics/service/AnalyticsService.java
@@ -2,7 +2,6 @@ package com.cosmetics.service;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 
--- src/main/java/com/cosmetics/service/impl/AnalyticsServiceImpl.java
+++ src/main/java/com/cosmetics/service/impl/AnalyticsServiceImpl.java
@@ -14,7 +14,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
