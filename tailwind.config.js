ef8b34d5a4b1648dd1032e91e02c79ff675e5f1f
Samin Ahmed <s.ahmedd99@gmail.com>
2025-07-27 23:26:32 +0100

    - Refactor: repalced tailwind CSS CDN with local stylesheet - added a new tailwind CSS config file (tailwind.config.js) to manage styles

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/main/resources/templates/**/*.html",
+    "./src/main/resources/static/components/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
